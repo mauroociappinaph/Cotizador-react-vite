f8e398f679ab1ee728d1e02876d29929487db74a
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-06-07 15:13:17 -0300

    Add files via upload
    Evento onChange  en el input de range.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 
 function App() {
+
+  //NOTE - Cantidad Inicial de dinero. Y su función cual lo modifica.
+  const [cantidad, setCantidad] = useState(10000);
+
+  //NOTE - Evento OnChange
+  const handleChange = (e) => {
+    console.log(+e.target.value);
+  }
+
   return <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+    <input 
+    type="range"
+    className="w-full h-6 bg-slate-50 accent-lime-500 hover:accent-lime-600"
+    onChange={handleChange}
+    />
   </div>;
 }
 
