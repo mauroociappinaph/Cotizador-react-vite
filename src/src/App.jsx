344b10ad31ec89079f0108cb67133a705f90e954
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-06-07 14:50:17 -0300

    Add files via upload
    Se agregó el input range.

---
+++ src/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+    <input 
+    type="range"
+    className="w-full h-6 bg-slate-50 accent-lime-500 hover:accent-lime-600"
+    />
+  </div>;
+}
+
+export default App;
