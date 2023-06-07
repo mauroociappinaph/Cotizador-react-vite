9952db2db6fab4cd6a244acea106ba1315d4c498
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-06-07 13:47:05 -0300

    Add files via upload
    Creación del index.css y modificación de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@
 function App() {
 
   return (
-
-<div className="my-20 max-w-lg mx-auto bg-white shadow p-10"></div>
+<h1>Cotizador-react-vite</h1>
+//<div className="my-20 max-w-lg mx-auto bg-white shadow p-10"></div>
 
   )
 
