dfd0f30b79a8dd40d54b69b670db016b737b88f1
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2023-06-08 10:26:23 -0300

    Add files via upload
    Select para plazo a pagar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Button from "./components/Button";
-import {formatearDinero} from "./helpers/index";
+import { formatearDinero } from "./helpers/index";
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
@@ -56,6 +56,17 @@ function App() {
       <p className="text-4xl font-extrabold text-center text-gray-800">
         {formatearDinero(cantidad)}
       </p>
+
+      <h2 className="text-2xl font-bold text-center text-gray-500">
+        Elige un <span className="text-indigo-500">Plazo</span> a pagar
+      </h2>
+
+      <select className="mt-5 w-full p-2 bg-white border-2 border-gray-300 rounded-lg text-center text-xl font-bold">
+        <option value="6">6</option>
+        <option value="12">12</option>
+        <option value="18">18</option>
+        <option value="24">24</option>
+      </select>
     </div>
   );
 }
